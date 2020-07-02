e07652d1ca637eab3ef3d18478268f7cbfbc1402
Steffen <per.sc@di.ku.dk>
2020-07-02 16:06:00 +0200

    registration test

--- scripts/test_registration.sh
+++ scripts/test_registration.sh
@@ -1,1 +1,7 @@
-python3 -m src.test_registration
+python3 -m src.test_registration --weights ./weights/phc-u373/registration/ncc/weights.ckpt --out ./out/phc-u373/registration/ncc.tif
+python3 -m src.test_registration --weights ./weights/phc-u373/registration/ncc+supervised/weights.ckpt --out ./out/phc-u373/registration/ncc+supervised.tif
+python3 -m src.test_registration --weights ./weights/phc-u373/registration/deepsim/weights.ckpt --out ./out/phc-u373/registration/deepsim.tif
+
+python3 -m src.test_registration --weights ./weights/platelet-em/registration/ncc/weights.ckpt --out ./out/platelet-em/registration/ncc.tif
+python3 -m src.test_registration --weights ./weights/platelet-em/registration/ncc+supervised/weights.ckpt --out ./out/platelet-em/registration/ncc+supervised.tif
+python3 -m src.test_registration --weights ./weights/platelet-em/registration/deepsim/weights.ckpt --out ./out/platelet-em/registration/deepsim.tif
