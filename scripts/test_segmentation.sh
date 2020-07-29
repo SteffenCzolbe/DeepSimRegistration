7cb12bf236a6825b133d685d607ae27d12ca6c36
Steffen <per.sc@di.ku.dk>
2020-07-29 15:07:16 +0200

    type

--- scripts/test_segmentation.sh
+++ scripts/test_segmentation.sh
@@ -1,3 +1,3 @@
 python3 -m src.test_2d_segmentation --weights ./weights/platelet-em/segmentation/weights.ckpt --out ./out/platelet-em/segmentation/segmentation.tif
 python3 -m src.test_2d_segmentation --weights ./weights/phc-u373/segmentation/weights.ckpt --out ./out/phc-u373/segmentation/segmentation.tif
-python3 -m src.test_3d_segmentation --weights ./weights/brain-mri/segmentation/weights.ckpt --out ./out/brain-mri/segmentation
+python3 -m src.test_3d_segmentation --weights ./weights/brain-mri/segmentation/weights.ckpt --out ./out/brain-mri/segmentation/
