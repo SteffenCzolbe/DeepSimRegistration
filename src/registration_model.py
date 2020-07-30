23aeeb64902d713cd09238683d49540fd282cc4b
Steffen <per.sc@di.ku.dk>
2020-07-30 15:39:45 +0200

    l2 loss added

--- src/registration_model.py
+++ src/registration_model.py
@@ -31,14 +31,15 @@ class RegistrationModel(CommonLightningModel):
             dropout=self.hparams.dropout,
         )
 
-        if hparams.loss == 'deepsim':
+        if hparams.loss.lower() == 'deepsim':
             if not hparams.deepsim_weights:
                 raise ValueError('No weights specified for Deep Similarity Metric.')
-            elif hparams.deepsim_weights.lower() == 'vgg':
-                feature_extractor = VGGFeatureExtractor()
             else:
                 feature_extractor = SegmentationModel.load_from_checkpoint(hparams.deepsim_weights)
-            self.deepsim = DeepSim(feature_extractor)
+                self.deepsim = DeepSim(feature_extractor)
+        elif hparams.loss.lower() == 'vgg':
+                feature_extractor = VGGFeatureExtractor()
+                self.vgg_loss = DeepSim(feature_extractor)
 
         self.ncc = NCC(window=hparams.ncc_win_size)
         self.mse = torch.nn.MSELoss()
@@ -55,12 +56,16 @@ class RegistrationModel(CommonLightningModel):
         return self.net(moving, fixed)
 
     def similarity_loss(self, I_m, I_1, S_m_onehot, S_1_onehot):
-        if self.hparams.loss == 'deepsim':
+        if self.hparams.loss.lower() == 'deepsim':
             return self.deepsim(I_m, I_1)
-        elif self.hparams.loss == 'ncc':
+        elif self.hparams.loss.lower() == 'vgg':
+            return self.vgg_loss(I_m, I_1)
+        elif self.hparams.loss.lower() == 'ncc':
             return self.ncc(I_m, I_1)
-        elif self.hparams.loss == 'ncc+supervised':
+        elif self.hparams.loss.lower() == 'ncc+supervised':
             return self.ncc(I_m, I_1) + self.mse(S_m_onehot, S_1_onehot)
+        elif self.hparams.loss.lower() == 'l2':
+            return self.mse(I_m, I_1)
         else:
             raise ValueError(f'loss function "{self.hparams.loss}" unknow.')
 
@@ -152,13 +157,13 @@ class RegistrationModel(CommonLightningModel):
         common_parser = CommonLightningModel.add_common_model_args()
         parser = argparse.ArgumentParser(parents=[common_parser, parent_parser], add_help=False)
         parser.add_argument(
-            "--loss", type=str, default='ncc', help="Similarity Loss function. Options: 'ncc', 'deepsim', 'ncc+supervised' (Default: ncc)"
+            "--loss", type=str, default='ncc', help="Similarity Loss function. Options: 'l2', 'ncc', 'deepsim', 'ncc+supervised', 'vgg' (Default: ncc)"
         )
         parser.add_argument(
             "--ncc_win_size", type=int, default=9, help="Window-Size for the NCC loss function (Default: 9)"
             )
         parser.add_argument(
-            "--deepsim_weights", type=str, default=None, help="Path to deep feature model weights, OR: 'vgg'"
+            "--deepsim_weights", type=str, default=None, help="Path to deep feature model weights"
         )
         parser.add_argument(
             "--lam", type=float, default=0.5, help="Diffusion regularizer strength"
