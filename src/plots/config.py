8ae6087b26f6a85c77cf5ca7084cf682ac3669df
Steffen <per.sc@di.ku.dk>
2020-07-31 16:48:26 +0200

    l2 > mse rename

--- src/plots/config.py
+++ src/plots/config.py
@@ -9,7 +9,7 @@ PLOT_CONFIG = {'phc-u373':{'display_name':'PhC-U373',
                            'smoothing_factor': 0.1},
                            }
 LOSS_FUNTION_ORDER = ['ncc', 'ncc+supervised', 'deepsim', 'transfer', 'vgg']
-LOSS_FUNTION_CONFIG = {'l2':{'display_name': 'L2',
+LOSS_FUNTION_CONFIG = {'l2':{'display_name': 'MSE',
                              'primary_color': plt.get_cmap('tab20c').colors[0]},
                        'ncc':{'display_name': 'NCC',
                              'primary_color': plt.get_cmap('tab20c').colors[1]},
