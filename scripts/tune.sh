9fdbb50b7b63c5107c966cf3668330786615d2ac
Steffen <per.sc@di.ku.dk>
2020-07-31 16:28:41 +0200

    restored

--- scripts/tune.sh
+++ scripts/tune.sh
@@ -111,8 +111,8 @@ if $TUNE_BRAIN; then
     done
 
     # deepsim
-    #for LAM in 0.25 0.5 1
-    #do
-    #$WRAPPER_FUNC python3 -m src.train_registration --dataset brain-mri --loss deepsim --ncc_win_size 9 --deepsim_weights ./weights/brain-mri/segmentation/weights.ckpt --lam $LAM --channels 32 64 128 --batch_size 1 --gpus -1 --lr 0.0001 --bnorm --dropout --accumulate_grad_batches 4 --max_epochs=150000
-    #done
+    for LAM in 0.25 0.5 1
+    do
+    $WRAPPER_FUNC python3 -m src.train_registration --dataset brain-mri --loss deepsim --ncc_win_size 9 --deepsim_weights ./weights/brain-mri/segmentation/weights.ckpt --lam $LAM --channels 32 64 128 --batch_size 1 --gpus -1 --lr 0.0001 --bnorm --dropout --accumulate_grad_batches 4 --max_epochs=150000
+    done
 fi
