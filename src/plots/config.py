0d0e85a1971bce4dee5837ba4ee1c3a94e0be7fb
Steffen <per.sc@di.ku.dk>
2020-08-19 16:35:50 +0200

    added hear-mri dataset (segmentation only)

--- src/plots/config.py
+++ src/plots/config.py
@@ -8,7 +8,7 @@ PLOT_CONFIG = {'phc-u373':{'display_name':'PhC-U373',
                'brain-mri':{'display_name':'Brain-MRI',
                            'smoothing_factor': 0.0},
                            }
-LOSS_FUNTION_ORDER = ['l2', 'ncc', 'ncc+supervised', 'deepsim'] #, 'transfer', 'vgg']
+LOSS_FUNTION_ORDER = ['l2', 'ncc', 'transfer', 'ncc+supervised', 'deepsim'] #, 'vgg']
 LOSS_FUNTION_CONFIG = {'l2':{'display_name': 'MSE',
                              'primary_color': plt.get_cmap('tab20c').colors[0]},
                        'ncc':{'display_name': 'NCC',
