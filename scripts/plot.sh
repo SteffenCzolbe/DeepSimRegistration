702fd4cc2067c54c25de94d17a2d3d68edd202fe
Steffen <per.sc@di.ku.dk>
2020-08-25 14:19:02 +0200

    statistical significance test

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,4 +1,11 @@
 python3 -m src.plots.convergence
 python3 -m src.plots.run_models
+python3 -m src.plots.statistical_significance_test
 python3 -m src.plots.test_score
 python3 -m src.plots.test_score_bar
+
+# crop pdfs
+
+pdfcrop ./out/plots/convergence.pdf ./out/plots/convergence.pdf 
+pdfcrop ./out/plots/test_score.pdf ./out/plots/test_score.pdf 
+pdfcrop ./out/plots/test_score_bar.pdf ./out/plots/test_score_bar.pdf 
