f3046fe00deb8ed6f31e8b738d614b2db918db6f
Steffen <per.sc@di.ku.dk>
2020-08-25 14:19:02 +0200

    statistical significance test

--- src/plots/config.py
+++ src/plots/config.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-DATASET_ORDER = ['platelet-em', 'phc-u373', 'brain-mri']
+DATASET_ORDER = ['brain-mri', 'platelet-em', 'phc-u373']
 PLOT_CONFIG = {'phc-u373':{'display_name':'PhC-U373',
                            'smoothing_factor': 0.97},
                'platelet-em':{'display_name':'Platelet-EM',
@@ -8,7 +8,7 @@ PLOT_CONFIG = {'phc-u373':{'display_name':'PhC-U373',
                'brain-mri':{'display_name':'Brain-MRI',
                            'smoothing_factor': 0.0},
                            }
-LOSS_FUNTION_ORDER = ['l2', 'ncc', 'transfer', 'ncc+supervised', 'deepsim'] #, 'vgg']
+LOSS_FUNTION_ORDER = ['l2', 'ncc', 'ncc+supervised', 'deepsim', 'transfer', 'vgg']
 LOSS_FUNTION_CONFIG = {'l2':{'display_name': 'MSE',
                              'primary_color': plt.get_cmap('tab20c').colors[0]},
                        'ncc':{'display_name': 'NCC',
