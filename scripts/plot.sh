477b4e1f6052a237fda1bc9e5249bd13a899bc70
Steffen <per.sc@di.ku.dk>
2020-08-25 17:53:47 +0200

    brain regions plot automated

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -3,9 +3,9 @@ python3 -m src.plots.run_models
 python3 -m src.plots.statistical_significance_test
 python3 -m src.plots.test_score
 python3 -m src.plots.test_score_bar
+python3 -m src.plots.test_score_per_class
 
 # crop pdfs
-
 pdfcrop ./out/plots/convergence.pdf ./out/plots/convergence.pdf 
 pdfcrop ./out/plots/test_score.pdf ./out/plots/test_score.pdf 
 pdfcrop ./out/plots/test_score_bar.pdf ./out/plots/test_score_bar.pdf 
