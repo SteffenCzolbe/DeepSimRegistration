3f1215b69b77455a15cb4534bc83dae8e02daee5
Steffen <per.sc@di.ku.dk>
2020-09-22 17:43:37 +0200

    adds brain-mri feature maps

--- src/plots/feature_maps.py
+++ src/plots/feature_maps.py
@@ -31,7 +31,7 @@ def crop(x_low, x_high, y_low, y_high, I):
     return I[:, :, x_low:x_high, y_low:y_high]
 
 
-def save_feature_map(save_path, feat, fullsize):
+def save_feature_map_platelet(save_path, feat, fullsize):
     crop_area_fullsize = (370, 650, 450, 730)
     this_size = feat.shape[-2:]
     shrink_factor = fullsize[0] / this_size[0]
@@ -44,31 +44,47 @@ def save_feature_map(save_path, feat, fullsize):
     # iterate over channels
     for i, feat_map in enumerate(feat):
         viz.export_img_2d(os.path.join(save_path, f'{i}.png'), feat_map.unsqueeze(0), normalize=True)
+        
+
+def save_feature_map_brain_mri(save_path, feat, fullsize):
+    # remove batch
+    D = feat.shape[3]
+    feat = feat[0, :, :, int(D*0.42), :]
+    
+    # iterate over channels
+    for i, feat_map in enumerate(feat):
+        viz.export_img_2d(os.path.join(save_path, f'{i}.png'), feat_map.unsqueeze(0), normalize=True)
     
 
 
-dataset = "platelet-em"
+datasets = ["platelet-em", "brain-mri"]
 loss_function = "deepsim"
 sample_idx = 10
 
-model_path = os.path.join("./weights/", dataset, "registration", loss_function)
+for dataset in datasets:
+    if dataset == 'platelet-em':
+        save_function = save_feature_map_platelet
+    elif dataset == 'brain-mri':
+        save_function = save_feature_map_brain_mri
+        
+    model_path = os.path.join("./weights/", dataset, "registration", loss_function)
 
-# load model
-checkpoint_path = os.path.join(model_path, "weights.ckpt")
-model = RegistrationModel.load_from_checkpoint(checkpoint_path=checkpoint_path)
+    # load model
+    checkpoint_path = os.path.join(model_path, "weights.ckpt")
+    model = RegistrationModel.load_from_checkpoint(checkpoint_path=checkpoint_path)
 
-# run model
-feats = get_feature_maps(model, sample_idx)
+    # run model
+    feats = get_feature_maps(model, sample_idx)
 
-fullsize = feats[0].shape[-2:]
+    fullsize = feats[0].shape[-2:]
 
 
-save_path = "./out/plots/platelet-featuremaps/"
-os.makedirs(save_path, exist_ok=True)
+    save_path = f"./out/plots/{dataset}-featuremaps/"
+    os.makedirs(save_path, exist_ok=True)
 
-# plot aligned image
-for i, feat in enumerate(feats):
-    layer_path = os.path.join(save_path, f'{i}')
-    os.makedirs(layer_path, exist_ok=True)
-    save_feature_map(layer_path, feat, fullsize)
+    # plot aligned image
+    for i, feat in enumerate(feats):
+        layer_path = os.path.join(save_path, f'{i}')
+        os.makedirs(layer_path, exist_ok=True)
+        save_function(layer_path, feat, fullsize)
 
