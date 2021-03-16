783c4fa55acc3a1686912614ec04e7be211bad21
Steffen <per.sc@di.ku.dk>
2021-03-16 14:15:32 +0100

    added determinant of jacobian

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,11 +1,12 @@
 python3 -m src.plots.convergence
-#python3 -m src.plots.run_models
+python3 -m src.plots.run_models
 python3 -m src.plots.statistical_significance_test
 python3 -m src.plots.test_score
 python3 -m src.plots.test_score_per_class
 python3 -m src.plots.hparam_tuning
 python3 -m src.plots.feature_maps
 python3 -m src.plots.img_sample
+python3 -m src.plots.metrics_to_csv
 
 # crop pdfs
 pdfcrop ./out/plots/convergence.pdf ./out/plots/convergence.pdf 
