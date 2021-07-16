8312a5cd7dc00d6376292d6a0f1854788783b052
Steffen <per.sc@di.ku.dk>
2021-07-16 14:05:54 +0200

    label resize

--- src/plots/convergence.py
+++ src/plots/convergence.py
@@ -13,7 +13,8 @@ def read_tb_scalar_logs(dir, scalar):
     """
     files = os.listdir(dir)
     log_files = list(
-        map(lambda f: os.path.join(dir, f), filter(lambda s: "events.out" in s, files))
+        map(lambda f: os.path.join(dir, f), filter(
+            lambda s: "events.out" in s, files))
     )
 
     # sort by timestamp, newer first (newer logs overwrite older ones in case of overlaps)
@@ -53,7 +54,7 @@ def read_tb_scalar_log(file, scalar, max_step=None):
     ea.Reload()  # loads events from file
     #step_to_epoch = ea.Scalars("epoch")
     #step_to_epoch = dict([(event.step, event.value) for event in step_to_epoch])
-    
+
     events = ea.Scalars(scalar)
 
     records = []
@@ -77,14 +78,15 @@ def smooth(ys, smoothing_factor=0.6):
 # set up sup-plots
 fig = plt.figure(figsize=(8.5, 2.5))
 axs = fig.subplots(1, len(DATASET_ORDER))
-plt.subplots_adjust(bottom=0.15)
+plt.subplots_adjust(bottom=0.18)
 
 for i, dataset in enumerate(DATASET_ORDER):
     axs[i].set_title(PLOT_CONFIG[dataset]["display_name"])
     for loss_function in tqdm(
         LOSS_FUNTION_ORDER, desc=f"plotting convergence of loss-finctions on {dataset}"
     ):
-        path = os.path.join("./weights/", dataset, "registration", loss_function)
+        path = os.path.join("./weights/", dataset,
+                            "registration", loss_function)
         if not os.path.isdir(path):
             continue
         x, y = read_tb_scalar_logs(path, "train/dice_overlap")
@@ -96,8 +98,10 @@ for i, dataset in enumerate(DATASET_ORDER):
         LOSS_FUNTION_CONFIG[loss_function]["handle"] = line[0]
 
 # add labels
-fig.text(0.5, 0.015, "Gradient Update Steps", ha="center", va="center")
-fig.text(0.07, 0.5, "Train Mean Dice Overlap", ha="center", va="center", rotation="vertical")
+fig.text(0.5, 0.03, "Gradient Update Steps",
+         ha="center", va="center", fontsize=16)
+fig.text(0.07, 0.5, "Train Mean Dice Overlap", ha="center",
+         va="center", rotation="vertical", fontsize=16)
 handles = [
     LOSS_FUNTION_CONFIG[loss_function]["handle"] for loss_function in LOSS_FUNTION_ORDER
 ]
@@ -114,4 +118,3 @@ for ax in axs:
 os.makedirs("./out/plots/", exist_ok=True)
 plt.savefig("./out/plots/convergence.pdf")
 plt.savefig("./out/plots/convergence.png")
-
