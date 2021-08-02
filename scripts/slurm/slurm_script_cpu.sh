465b9734a56dc2edf54be405720f0d1239f79328
Steffen <per.sc@di.ku.dk>
2021-08-02 17:04:18 +0200

    cpu memory finetuning

--- scripts/slurm/slurm_script_cpu.sh
+++ scripts/slurm/slurm_script_cpu.sh
@@ -8,7 +8,7 @@
 #SBATCH --job-name='unnamed job'
 
 # normal cpu stuff: allocate cpus, memory
-#SBATCH --ntasks=1 --cpus-per-task=2 --mem=52000M
+#SBATCH --ntasks=1 --cpus-per-task=2 --mem=40000M
 
 # we run on the cpu partition
 #SBATCH -p image1
