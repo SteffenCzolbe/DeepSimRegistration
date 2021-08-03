5d99744f9564b55974fa3f29bb1a87c7363449c8
Steffen <per.sc@di.ku.dk>
2021-08-03 18:19:22 +0200

    included SyN results

--- src/plots/config.py
+++ src/plots/config.py
@@ -23,6 +23,14 @@ LOSS_FUNTION_ORDER = [
     "deepsim",
 ]
 
+SYN_LOSS_FUNTIONS = [
+    "syn",
+    "syn_ae",
+    "syn_seg",
+]
+
+ALL_METHODS = LOSS_FUNTION_ORDER + [None] + SYN_LOSS_FUNTIONS
+
 EXTRACT_BEFORE_WARP_LOSS_FUNTIONS = [
     "deepsim-ae",
     "deepsim-ae-ebw",
@@ -34,67 +42,99 @@ LOSS_FUNTION_CONFIG = {
     "l2": {
         "display_name": "MSE",
         "primary_color": plt.get_cmap("tab20c").colors[0],
-        "marker": "s"},
+        "marker": "s",
+        "our_method": False},
     "ncc": {
         "display_name": "NCC",
         "primary_color": plt.get_cmap("tab20c").colors[1],
-        "marker": "^"},
+        "marker": "^",
+        "our_method": False},
     "ncc2": {
         "display_name": r"$\text{NCC}$",
         "display_name_bold": r"$\textbf{NCC}$",
         "primary_color": plt.get_cmap("tab20c").colors[1],
-        "marker": "^"
+        "marker": "^",
+        "our_method": False
     },
     "ncc+supervised": {
         "display_name": r"$\text{NCC}_{\text{sup}}$",
         "display_name_bold": r"$\textbf{NCC}_{\textbf{sup}}$",
         "primary_color": plt.get_cmap("tab20c").colors[2],
-        "marker": ">"
+        "marker": ">",
+        "our_method": False
     },
     "ncc2+supervised": {
         "display_name": r"$\text{NCC}_{\text{sup}}$",
         "display_name_bold": r"$\textbf{NCC}_{\textbf{sup}}$",
         "primary_color": plt.get_cmap("tab20c").colors[2],
-        "marker": ">"
+        "marker": ">",
+        "our_method": False
     },
     "nmi": {
         "display_name": r"$\text{NMI}$",
         "display_name_bold": r"$\textbf{NMI}$",
         "primary_color": plt.get_cmap("tab20b").colors[2],
-        "marker": "v"},
+        "marker": "v",
+        "our_method": False},
     "vgg": {
         "display_name": r"$\text{VGG}$",
         "display_name_bold": r"$\textbf{VGG}$",
         "primary_color": plt.get_cmap("tab20c").colors[3],
-        "marker": "<"
+        "marker": "<",
+        "our_method": False
     },
     "deepsim": {
         "display_name": r"$\text{DeepSim}_{\text{seg}}$",
         "display_name_bold": r"$\textbf{DeepSim}_{\textbf{seg}}$",
         "primary_color": plt.get_cmap("tab20c").colors[4],
-        "marker": "x"
+        "marker": "x",
+        "our_method": True
     },
     "deepsim-ae": {
         "display_name": r"$\text{DeepSim}_{\text{ae}}$",
         "display_name_bold": r"$\textbf{DeepSim}_{\textbf{ae}}$",
         "primary_color": plt.get_cmap("tab20c").colors[5],
-        "marker": "o"
+        "marker": "o",
+        "our_method": True
     },
     "transfer": {
         "display_name": "Transfer",
         "primary_color": plt.get_cmap("tab20c").colors[16],
-        "marker": "x"
+        "marker": "x",
+        "our_method": True
     },
     "deepsim-ebw": {
         "display_name": r"$\text{DeepSim}_{\text{seg}}\text{(EbT)}$",
         "display_name_bold": r"$\textbf{DeepSim}_{\textbf{seg}}\text{(EbT)}$",
         "primary_color": plt.get_cmap("tab20c").colors[6],
-        "marker": "P"
+        "marker": "x",
+        "our_method": True
     },
     "deepsim-ae-ebw": {
         "display_name": r"$\text{DeepSim}_{\text{ae}}\text{(EbT)}$",
         "display_name_bold": r"$\textbf{DeepSim}_{\textbf{ae}}\text{(EbT)}$",
         "primary_color": plt.get_cmap("tab20c").colors[7],
-        "marker": "D"
+        "marker": "o",
+        "our_method": True
+    },
+    "syn": {
+        "display_name": r"$\text{SyN}$",
+        "feature_extractor": "none",
+        "primary_color": plt.get_cmap("tab20c").colors[18],
+        "our_method": False,
+    },
+    "syn_ae": {
+        "display_name": r"$\text{SyN + }\text{DeepSim}_{\text{ae}}$",
+        "display_name_bold": r"$\textbf{SyN + }\textbf{DeepSim}_{\text{ae}}$",
+        "feature_extractor": "ae",
+        "primary_color": plt.get_cmap("tab20c").colors[17],
+        "our_method": True,
+    },
+    "syn_seg": {
+        "display_name": r"$\text{SyN + }\text{DeepSim}_{\text{seg}}$",
+        "display_name_bold": r"$\textbf{SyN + }\textbf{DeepSim}_{\text{seg}}$",
+        "feature_extractor": "seg",
+        "primary_color": plt.get_cmap("tab20c").colors[16],
+        "our_method": True,
     },
 }
--- src/plots/test_score.py
+++ src/plots/test_score.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+import yaml
 import numpy as np
 from tqdm import tqdm
 from src.registration_model import RegistrationModel
@@ -8,14 +9,69 @@ from .config import *
 from .run_models import run_models
 from matplotlib.ticker import FormatStrFormatter
 
+
+def get_score_and_label(dataset, loss_function):
+    if loss_function is None:
+        return get_empty_score_and_label()
+    if loss_function in LOSS_FUNTION_ORDER:
+        return get_score_and_label_for_dl_method(dataset, loss_function)
+    else:
+        return get_score_and_label_for_syn(dataset, loss_function)
+
+
+def get_empty_score_and_label():
+    bar_color = "black"
+    label = ""
+    label_color = "black"
+    scores = []
+    return scores, bar_color, label, label_color
+
+
+def get_score_and_label_for_syn(dataset, loss_function):
+    dataset_path = os.path.join('./out', dataset, 'syn')
+    fnames = os.listdir(dataset_path)
+    scores = {}
+
+    # read scores
+    for fname in fnames:
+        with open(os.path.join(dataset_path, fname), 'r') as file:
+            data = yaml.safe_load(file)
+            feature_extractor = data['hparams']['feature_extractor']
+            scores[feature_extractor] = data['scores']
+
+    # filter out images where one model is missing a score (helps with partial evaluation during development)
+    feature_extractors = scores.keys()
+    idxs = [set(scores[fe].keys()) for fe in feature_extractors]
+    common_idxs = set.intersection(* idxs)
+    for feature_extractor in feature_extractors:
+        scores[feature_extractor] = np.array([scores[feature_extractor][idx]
+                                              for idx in common_idxs])
+    bar_color = LOSS_FUNTION_CONFIG[loss_function]["primary_color"]
+    is_our_method = LOSS_FUNTION_CONFIG[loss_function]["our_method"]
+    label = LOSS_FUNTION_CONFIG[loss_function]["display_name_bold" if is_our_method else "display_name"]
+    label_color = "black"
+    return scores[LOSS_FUNTION_CONFIG[loss_function]["feature_extractor"]], bar_color, label, label_color
+
+
+def get_score_and_label_for_dl_method(dataset, loss_function):
+    results = run_models(use_cached=True)
+    if loss_function not in results[dataset].keys():
+        return False
+    scores = results[dataset][loss_function]["dice_overlap"]
+    bar_color = LOSS_FUNTION_CONFIG[loss_function]["primary_color"]
+    label_color = "black"
+    is_our_method = LOSS_FUNTION_CONFIG[loss_function]["our_method"]
+    label = LOSS_FUNTION_CONFIG[loss_function]["display_name_bold" if is_our_method else "display_name"]
+    return scores, bar_color, label, label_color
+
+
 # set up sup-plots
-fig = plt.figure(figsize=(7, 2.5))
+fig = plt.figure(figsize=(10, 3.5))
 axs = fig.subplots(1, len(DATASET_ORDER))
-plt.subplots_adjust(bottom=0.32, wspace=0.3)
+plt.subplots_adjust(bottom=0.4, wspace=0.2)
 plt.rcParams["boxplot.medianprops.color"] = "k"
 plt.rcParams["boxplot.medianprops.linewidth"] = 3.0
 outlier_porps = dict(markersize=3, markeredgecolor="black", marker='.')
-results = run_models(use_cached=True)
 for i, dataset in enumerate(DATASET_ORDER):
     # set dataset title
     axs[i].set_title(PLOT_CONFIG[dataset]["display_name"])
@@ -24,24 +80,16 @@ for i, dataset in enumerate(DATASET_ORDER):
     label_colors = []
     label_bold_font = []
     colors = []
-    for loss_function in LOSS_FUNTION_ORDER:
+    for loss_function in ALL_METHODS:
         # test model
-        if loss_function in results[dataset].keys():
-            mean_dice_overlaps.append(results[dataset][loss_function]["dice_overlap"])
-            rank = results[dataset][loss_function]["rank"]
-            if rank == 0:
-                label_colors.append("black")
-                label_bold_font.append(True)
-                labels.append(LOSS_FUNTION_CONFIG[loss_function]["display_name_bold"])
-            elif rank == 1:
-                label_colors.append("black")
-                label_bold_font.append(False)
-                labels.append(LOSS_FUNTION_CONFIG[loss_function]["display_name"])
-            else:
-                label_colors.append('#404040')
-                label_bold_font.append(False)
-                labels.append(LOSS_FUNTION_CONFIG[loss_function]["display_name"])
-            colors.append(LOSS_FUNTION_CONFIG[loss_function]["primary_color"])
+        r = get_score_and_label(dataset, loss_function)
+        if not r:
+            continue
+        scores, bar_color, label, label_color = r
+        mean_dice_overlaps.append(scores)
+        colors.append(bar_color)
+        labels.append(label)
+        label_colors.append(label_color)
 
     # plot boxes.
     if len(labels) > 0:
@@ -56,21 +104,26 @@ for i, dataset in enumerate(DATASET_ORDER):
         for patch, color in zip(bplot["boxes"], colors):
             patch.set_facecolor(color)
 
-        # rotate labels
+        # rotate and color labels
         for tick, color in zip(axs[i].get_xticklabels(), label_colors):
-            tick.set_rotation(60)
+            tick.set_rotation(80)
             tick.set_color(color)
-            
-    
+
+        # hide empty ticks
+        for j, label in enumerate(labels):
+            if label is None or label == "":
+                axs[i].xaxis.majorTicks[j].set_visible(False)
+
+
 # configure axis precision
 for ax in axs:
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 
 
 # add labels
-fig.text(0.06, 0.5, "Test Mean Dice Overlap", ha="center", va="center", rotation="vertical")
+fig.text(0.075, 0.63, "Test Mean Dice Overlap",
+         ha="center", va="center", rotation="vertical")
 
 os.makedirs("./out/plots/", exist_ok=True)
 plt.savefig("./out/plots/test_score.pdf")
 plt.savefig("./out/plots/test_score.png")
-
