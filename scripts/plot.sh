40f465a58a4059bbbc47e7cd6b25a8bddbe29e2d
Paraskevas Pegios <ppar@phoebe.imm.dtu.dk>
2022-05-16 18:30:20 +0200

    sure

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -23,19 +23,19 @@ python3 -m src.plots.tune_levels --deepsim seg
 python3 -m src.plots.tune_transfer_learning
 python3 -m src.plots.tune_extract_before_warp
 
-# crop pdfs
-pdfcrop ./out/plots/pdf/convergence_train.pdf ./out/plots/pdf/convergence_train.pdf
-pdfcrop ./out/plots/pdf/convergence_val.pdf ./out/plots/pdf/convergence_val.pdf
-pdfcrop ./out/plots/pdf/extract_before_warp.pdf ./out/plots/pdf/extract_before_warp.pdf
-pdfcrop ./out/plots/pdf/img_sample_detail_all.pdf ./out/plots/pdf/img_sample_detail_all.pdf 
-pdfcrop ./out/plots/pdf/img_sample.pdf ./out/plots/pdf/img_sample.pdf 
-pdfcrop ./out/plots/pdf/levels_ae.pdf ./out/plots/pdf/levels_seg.pdf
-pdfcrop ./out/plots/pdf/loss_sampleA.pdf ./out/plots/pdf/loss_sampleA.pdf
-pdfcrop ./out/plots/pdf/loss_sampleB.pdf ./out/plots/pdf/loss_sampleB.pdf 
-pdfcrop ./out/plots/pdf/smoothness_vs_dice_overlap_transmorph.pdf ./out/plots/pdf/smoothness_vs_dice_overlap_transmorph.pdf 
-pdfcrop ./out/plots/pdf/smoothness_vs_dice_overlap_voxelmorph.pdf ./out/plots/pdf/smoothness_vs_dice_overlap_voxelmorph.pdf 
-pdfcrop ./out/plots/pdf/test_score_per_class_brain-mri.pdf ./out/plots/pdf/test_score_per_class_brain-mri.pdf 
-pdfcrop ./out/plots/pdf/test_score.pdf ./out/plots/pdf/test_score.pdf 
-pdfcrop ./out/plots/pdf/transfer_learing.pdf ./out/plots/pdf/transfer_learing.pdf 
-pdfcrop ./out/plots/pdf/transmorph.pdf ./out/plots/pdf/transmorph.pdf 
-pdfcrop ./out/plots/pdf/voxelmorph.pdf ./out/plots/pdf/voxelmorph.pdf 
+# # crop pdfs for paper
+# pdfcrop ./out/plots/pdf/convergence_train.pdf ./out/plots/pdf/convergence_train.pdf
+# pdfcrop ./out/plots/pdf/convergence_val.pdf ./out/plots/pdf/convergence_val.pdf
+# pdfcrop ./out/plots/pdf/extract_before_warp.pdf ./out/plots/pdf/extract_before_warp.pdf
+# pdfcrop ./out/plots/pdf/img_sample_detail_all.pdf ./out/plots/pdf/img_sample_detail_all.pdf 
+# pdfcrop ./out/plots/pdf/img_sample.pdf ./out/plots/pdf/img_sample.pdf 
+# pdfcrop ./out/plots/pdf/levels_ae.pdf ./out/plots/pdf/levels_seg.pdf
+# pdfcrop ./out/plots/pdf/loss_sampleA.pdf ./out/plots/pdf/loss_sampleA.pdf
+# pdfcrop ./out/plots/pdf/loss_sampleB.pdf ./out/plots/pdf/loss_sampleB.pdf 
+# pdfcrop ./out/plots/pdf/smoothness_vs_dice_overlap_transmorph.pdf ./out/plots/pdf/smoothness_vs_dice_overlap_transmorph.pdf 
+# pdfcrop ./out/plots/pdf/smoothness_vs_dice_overlap_voxelmorph.pdf ./out/plots/pdf/smoothness_vs_dice_overlap_voxelmorph.pdf 
+# pdfcrop ./out/plots/pdf/test_score_per_class_brain-mri.pdf ./out/plots/pdf/test_score_per_class_brain-mri.pdf 
+# pdfcrop ./out/plots/pdf/test_score.pdf ./out/plots/pdf/test_score.pdf 
+# pdfcrop ./out/plots/pdf/transfer_learing.pdf ./out/plots/pdf/transfer_learing.pdf 
+# pdfcrop ./out/plots/pdf/transmorph.pdf ./out/plots/pdf/transmorph.pdf 
+# pdfcrop ./out/plots/pdf/voxelmorph.pdf ./out/plots/pdf/voxelmorph.pdf 
