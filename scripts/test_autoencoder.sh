1b84363ccca115e0f7d37b686da24d6bbb68d54c
wjq874 <wjq874@a00552.science.domain>
2022-11-30 14:57:47 +0100

    test scripts for seg and ae of hippocampusmr dataset

--- scripts/test_autoencoder.sh
+++ scripts/test_autoencoder.sh
@@ -1,1 +1,2 @@
 python3 -m src.test_3d_autoencoder --weights ./weights/brain-mri/autoencoder/weights.ckpt --out ./out/brain-mri/autoencoder/
+python3 -m src.test_3d_autoencoder --weights ./weights/hippocampusmr/autoencoder/weights.ckpt --out ./out/hippocampusmr/autoencoder/
--- scripts/test_segmentation.sh
+++ scripts/test_segmentation.sh
@@ -1,4 +1,4 @@
 python3 -m src.test_2d_segmentation --weights ./weights/platelet-em/segmentation/weights.ckpt --out ./out/platelet-em/segmentation/segmentation.tif
 python3 -m src.test_2d_segmentation --weights ./weights/phc-u373/segmentation/weights.ckpt --out ./out/phc-u373/segmentation/segmentation.tif
 python3 -m src.test_3d_segmentation --weights ./weights/brain-mri/segmentation/weights.ckpt --out ./out/brain-mri/segmentation/
-python3 -m src.test_3d_segmentation --weights ./weights/heart-mri/segmentation/weights.ckpt --out ./out/heart-mri/segmentation/
+python3 -m src.test_3d_segmentation --weights ./weights/hippocampusmr/segmentation/weights.ckpt --out ./out/hippocampusmr/segmentation/
